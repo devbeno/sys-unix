698883f372c09ffa170c22cd0db6d03fb9a6ae1a
Benjamin <benjophp@gmail.com>
2022-11-02 08:40:22 +0100

    Update devs.sh

--- devs.sh
+++ devs.sh
@@ -1,21 +1,47 @@
-pacstrap /mnt base base-devel linux linux-firmware amd-ucode btrfs-progs git go \
-    kanshi zstd iwd networkmanager mesa vulkan-radeon libva-mesa-driver openssh \
-    mesa-vdpau xf86-video-amdgpu docker libvirt qemu refind rustup wl-clipboard \
-    zsh sshguard npm bc ripgrep bat tokei hyperfine rust-analyzer xdg-user-dirs \
-    systemd-swap pigz pbzip2 snapper chrony noto-fonts a52dec faac iptables-nft \
-    tlp faad2 flac jasper grim libdca libdv libmad libmpeg2 libtheora libvorbis \
-    waybar wavpack xvidcore libde265 gstreamer gst-libav gst-plugins-bad breeze \
-    gst-plugins-base gst-plugins-good gst-plugins-ugly gstreamer-vaapi seahorse \
-    sway lollypop alacritty wofi polkit-gnome mako slurp xdg-desktop-portal-wlr \
-    gvfs libxv libsecret gnome-keyring nautilus nautilus-image-converter gdm fd \
-    xarchiver arj cpio lha udiskie nautilus-share nautilus-sendto imv mpv lrzip \
-    unrar zip chezmoi powertop brightnessctl lastpass-cli sbsigntools x264 lzip \
-    xorg-xwayland apparmor ttf-roboto ttf-roboto-mono ttf-dejavu ttf-liberation \
-    ttf-fira-code ttf-hanazono ttf-fira-mono seahorse-nautilus exa ttf-opensans \
-    pulseaudio lzop p7zip ttf-hack noto-fonts noto-fonts-emoji ttf-font-awesome \
-    ttf-droid adobe-source-code-pro-fonts firefox-decentraleyes libva-utils man \
-    firefox-dark-reader lame network-manager-applet unarj blueman yarn npm code \
-    firefox-ublock-origin irqbalance swayidle haveged profile-sync-daemon shfmt \
-    compsize pipewire-pulse pipewire-jack pipewire-alsa gnome-boxes wf-recorder \
-    dbus-broker wireplumber skim youtube-dl nftables python-nautilus celluloid \
-    entr reflector postgresql tmux gnome-podcasts
+#!/bin/bash
+
+# 4 - Create and Mount Subvolumes
+# Create subvolumes for root, home, the package cache, snapshots and the entire Btrfs file system
+mount /dev/mapper/crypt /mnt
+btrfs sub create /mnt/@
+btrfs sub create /mnt/@home
+btrfs sub create /mnt/@pkg
+btrfs sub create /mnt/@abs
+btrfs sub create /mnt/@tmp
+btrfs sub create /mnt/@srv
+btrfs sub create /mnt/@snapshots
+btrfs sub create /mnt/@btrfs
+btrfs sub create /mnt/@swap
+umount /mnt
+
+# Mount the subvolumes
+mount -o noatime,nodiratime,compress=zstd,commit=120,space_cache,ssd,discard=async,autodefrag,subvol=@ /dev/mapper/crypt /mnt
+mkdir -p /mnt/{boot,home,var/cache/pacman/pkg,.snapshots,.swapvol,btrfs}
+mount -o noatime,nodiratime,compress=zstd,commit=120,space_cache,ssd,discard=async,autodefrag,subvol=@home /dev/mapper/crypt /mnt/home
+mount -o noatime,nodiratime,compress=zstd,commit=120,space_cache,ssd,discard=async,autodefrag,subvol=@pkg /dev/mapper/crypt /mnt/var/cache/pacman/pkg
+mount -o noatime,nodiratime,compress=zstd,commit=120,space_cache,ssd,discard=async,autodefrag,subvol=@abs /dev/mapper/crypt /mnt/var/abs
+mount -o noatime,nodiratime,compress=zstd,commit=120,space_cache,ssd,discard=async,autodefrag,subvol=@tmp /dev/mapper/crypt /mnt/var/tmp
+mount -o noatime,nodiratime,compress=zstd,commit=120,space_cache,ssd,discard=async,autodefrag,subvol=@srv /dev/mapper/crypt /mnt/srv
+mount -o noatime,nodiratime,compress=zstd,commit=120,space_cache,ssd,discard=async,autodefrag,subvol=@snapshots /dev/mapper/crypt /mnt/.snapshots
+mount -o compress=no,space_cache,ssd,discard=async,subvol=@swap /dev/mapper/crypt /mnt/.swapvol
+mount -o noatime,nodiratime,compress=zstd,commit=120,space_cache,ssd,discard=async,autodefrag,subvolid=5 /dev/mapper/crypt /mnt/btrfs
+
+# Create Swapfile
+truncate -s 0 /mnt/.swapvol/swapfile
+chattr +C /mnt/.swapvol/swapfile
+btrfs property set /mnt/.swapvol/swapfile compression none
+fallocate -l 16G /mnt/.swapvol/swapfile
+chmod 600 /mnt/.swapvol/swapfile
+mkswap /mnt/.swapvol/swapfile
+swapon /mnt/.swapvol/swapfile
+
+# Mount the EFI partition
+mount /dev/nvme0n1p1 /mnt/boot
+
+
+pacstrap /mnt base base-devel linux linux-firmware amd-ucode btrfs-progs sbsigntools \
+    neovim zstd go iwd networkmanager mesa vulkan-radeon libva-mesa-driver mesa-vdpau \
+    xf86-video-amdgpu docker libvirt qemu openssh refind zsh zsh-completions \
+    zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting git \
+    pigz pbzip2
+
