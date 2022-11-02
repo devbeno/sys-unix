b7cc317cd39d4adbbc43df2f7d74dff37e777fb2
Benjamin <benjophp@gmail.com>
2022-11-02 07:14:18 +0100

    Update devs.sh

--- devs.sh
+++ devs.sh
@@ -1,10 +1,21 @@
-mount -o noatime,compress-force=zstd,commit=120,space_cache=v2,ssd,discard=async,autodefrag,subvol=@ /dev/mapper/crypt /mnt
-mkdir -p /mnt/{boot,home,var/cache,var/log,.snapshots,btrfs,var/tmp,var/abs,srv}
-mount -o noatime,compress-force=zstd,commit=120,space_cache=v2,ssd,discard=async,autodefrag,subvol=@home /dev/mapper/crypt /mnt/home  && \
-mount -o nodev,nosuid,noexec,noatime,compress-force=zstd,commit=120,space_cache=v2,ssd,discard=async,autodefrag,subvol=@abs /dev/mapper/crypt /mnt/var/abs && \
-mount -o nodev,nosuid,noexec,noatime,compress-force=zstd,commit=120,space_cache=v2,ssd,discard=async,autodefrag,subvol=@tmp /dev/mapper/crypt /mnt/var/tmp && \
-mount -o noatime,compress-force=zstd,commit=120,space_cache=v2,ssd,discard=async,autodefrag,subvol=@srv /dev/mapper/crypt /mnt/srv && \
-mount -o nodev,nosuid,noexec,noatime,compress-force=zstd,commit=120,space_cache=v2,ssd,discard=async,autodefrag,subvol=@log /dev/mapper/crypt /mnt/var/log && \
-mount -o nodev,nosuid,noexec,noatime,compress-force=zstd,commit=120,space_cache=v2,ssd,discard=async,autodefrag,subvol=@cache /dev/mapper/crypt /mnt/var/cache && \
-mount -o noatime,compress-force=zstd,commit=120,space_cache=v2,ssd,discard=async,autodefrag,subvol=@snapshots /dev/mapper/crypt /mnt/.snapshots && \
-mount -o noatime,compress-force=zstd,commit=120,space_cache=v2,ssd,discard=async,autodefrag,subvolid=5 /dev/mapper/crypt /mnt/btrfs
+pacstrap /mnt base base-devel linux linux-firmware amd-ucode btrfs-progs git go \
+    kanshi zstd iwd networkmanager mesa vulkan-radeon libva-mesa-driver openssh \
+    mesa-vdpau xf86-video-amdgpu docker libvirt qemu refind rustup wl-clipboard \
+    zsh sshguard npm bc ripgrep bat tokei hyperfine rust-analyzer xdg-user-dirs \
+    systemd-swap pigz pbzip2 snapper chrony noto-fonts a52dec faac iptables-nft \
+    tlp faad2 flac jasper grim libdca libdv libmad libmpeg2 libtheora libvorbis \
+    waybar wavpack xvidcore libde265 gstreamer gst-libav gst-plugins-bad breeze \
+    gst-plugins-base gst-plugins-good gst-plugins-ugly gstreamer-vaapi seahorse \
+    sway lollypop alacritty wofi polkit-gnome mako slurp xdg-desktop-portal-wlr \
+    gvfs libxv libsecret gnome-keyring nautilus nautilus-image-converter gdm fd \
+    xarchiver arj cpio lha udiskie nautilus-share nautilus-sendto imv mpv lrzip \
+    unrar zip chezmoi powertop brightnessctl lastpass-cli sbsigntools x264 lzip \
+    xorg-xwayland apparmor ttf-roboto ttf-roboto-mono ttf-dejavu ttf-liberation \
+    ttf-fira-code ttf-hanazono ttf-fira-mono seahorse-nautilus exa ttf-opensans \
+    pulseaudio lzop p7zip ttf-hack noto-fonts noto-fonts-emoji ttf-font-awesome \
+    ttf-droid adobe-source-code-pro-fonts firefox-decentraleyes libva-utils man \
+    firefox-dark-reader lame network-manager-applet unarj blueman yarn npm code \
+    firefox-ublock-origin irqbalance swayidle haveged profile-sync-daemon shfmt \
+    compsize pipewire-pulse pipewire-jack pipewire-alsa gnome-boxes wf-recorder \
+    dbus-broker wireplumber skim youtube-dl nftables python-nautilus celluloid \
+    entr reflector postgresql tmux gnome-podcasts
